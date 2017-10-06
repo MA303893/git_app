c9a10c47c870af730ef864619a637b0e4137f7c6
Manish Singh <manish.singh@jifflenow.com>
2017-10-06 12:22:09 +0530

    API support for devise

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,20 +1,67 @@
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  skip_before_action :authenticate_user!, :only => [:create, :new]
+  skip_before_action :verify_signed_out_user, :only => [:destroy], :if => Proc.new {|c| c.request.format == 'application/json'}
+  # skip_authorization_check only: [:create, :failure, :show_current_user, :options, :new]
+  respond_to :json, :html
+  def new
+    self.resource = resource_class.new(sign_in_params)
+    clean_up_passwords(resource)
+    respond_with(resource, serialize_options(resource))
+  end
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def create
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+    respond_to do |format|
+      format.html {
+        super
+      }
+      format.json {
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+        resource = resource_from_credentials
+        #build_resource
+        return invalid_login_attempt unless resource
+
+        if resource.valid_password?(params[:user][:password])
+          render :json => {user: {email: resource.email, :auth_token => resource.auth_token}}, success: true, status: :created
+        else
+          invalid_login_attempt
+        end
+      }
+    end
+  end
+
+  def destroy
+    respond_to do |format|
+      format.html {
+        super
+      }
+      format.json {
+        user = User.find_by_auth_token(request.headers['X-API-TOKEN'])
+        if user
+          user.reset_authentication_token!
+          render :json => {:message => 'Session deleted.'}, :success => true, :status => 200
+        else
+          render :json => {:message => 'Invalid token.'}, :status => 404
+        end
+      }
+    end
+  end
+
+  protected
+  def invalid_login_attempt
+    warden.custom_failure!
+    render json: {success: false, message: 'Error with your login or password'}, status: 401
+  end
+
+  def resource_from_credentials
+    data = {email: params[:user][:email]}
+    if res = resource_class.find_for_database_authentication(data)
+      if res.valid_password?(params[:user][:password])
+        res
+      end
+    end
+  end
 
   # protected
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171005123104) do
+ActiveRecord::Schema.define(version: 20171005164042) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20171005123104) do
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "auth_token"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
