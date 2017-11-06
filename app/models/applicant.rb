2e5e3278d5d38afa6494179f78998e6c127cce17
Manish Singh <manish.singh@jifflenow.com>
2017-11-06 20:45:17 +0530

    adding eval to subjects

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -117,7 +117,7 @@ class Applicant < ApplicationRecord
           name: q.name,
           place_of_study: q.place_of_study,
           country: q.country,
-          subjects: q.subjects,
+          subjects: eval(q.subjects) rescue nil,
           duration: q.duration,
           date_of_completion: q.date_of_completion
       }
