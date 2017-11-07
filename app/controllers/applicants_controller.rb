6eb3111534c1f289602dde1c233360d4e74d931f
Manish Singh <manish.singh@jifflenow.com>
2017-11-07 16:16:26 +0530

    adding initail controller for applicant dependent and others

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -1,6 +1,6 @@
 class ApplicantsController < ApplicationController
   before_action :set_applicant #, only: [:profile, :qualifications_and_licences]
-  skip_before_action :authenticate_user!
+  # skip_before_action :authenticate_user!
 
   def update_personal_details
     if @applicant
