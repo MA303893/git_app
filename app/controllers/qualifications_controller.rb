422a77450b1fa76a6f427ababdbd9aa40fdc05d1
Manish Singh <manish.singh@jifflenow.com>
2017-11-07 16:41:39 +0530

    adding CUDR for qualifications

--- app/controllers/qualifications_controller.rb
+++ app/controllers/qualifications_controller.rb
@@ -1,10 +1,38 @@
 class QualificationsController < ApplicationController
   include ApplicantConcern
-  def update_qualifications
-
-  end
 
   def create_qualification
+    params[:subjects] = params[:subjects].to_s
+    qual = Qualification.create_or_update_qualification(@applicant, params)
+    if qual
+      render :json => {qualification: qual.as_json, success: true}, success: true, status: 200
+    else
+      render :json => unsuccessful_response("Qualification could not be added! Please try again."), success: false, status: 400
+    end
+  end
 
+  def update_qualifications
+    params[:subjects] = params[:subjects].to_s
+    qual = Qualification.create_or_update_qualification(@applicant, params)
+    if qual
+      render :json => {qualification: qual.as_json, success: true}, success: true, status: 200
+    else
+      render :json => unsuccessful_response("Qualification could not be updated! Please try again."), success: false, status: 400
+    end
+  end
+
+  def destroy
+    if params[:id]
+      qual = Qualification.find_qualification_by_applicant(@applicant, params[:id])
+      if qual
+        qual.destroy
+        render :json => {qualification: qual.as_json, success: true, message: "Qualification deleted!"}, success: true, status: 200
+      else
+        render :json => unsuccessful_response("Qualification cannot be deleted. You are not authorized!"), success: false, status: 400
+      end
+
+    else
+      render :json => unsuccessful_response("Cannot delete qualification without ID."), success: false, status: 400
+    end
   end
 end
